00a442d1da29543570ba80f80a2c32f00c5d3f2d
News-s <84093019+news-s@users.noreply.github.com>
2024-11-22 18:12:56 +0100

    added two endpoints: get_

---
+++ app.py
@@ -0,0 +1,60 @@
+try:
+    from flask import *
+    import yfinance as yf
+    import config
+except:
+    import sys, os
+    os.system('pip install libs.txt')
+    print('Please re-run this code')
+    sys.exit(1)
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/get_data/<symbol>')
+def get_data(symbol):
+    try:
+        spolka = yf.Ticker("TSLA")
+        dane = spolka.history(period="1y", interval="1d")
+        wynik = {
+                "symbol": symbol,
+                "data": [
+                    {
+                        "date": str(index.date()),
+                        "open": row["Open"],
+                        "high": row["High"],
+                        "low": row["Low"],
+                        "close": row["Close"]
+                    }
+                    for index, row in dane.iterrows()
+                ]
+            }
+        return jsonify(wynik), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+@app.route('/price/<symbol>', methods=['GET'])
+def get_stock_price(symbol):
+    try:
+        # Pobierz dane o spółce
+        spolka = yf.Ticker(symbol)
+        aktualna_cena = spolka.history(period="1d")["Close"].iloc[-1]  # Ostatnia cena zamknięcia
+
+        # Przygotowanie odpowiedzi
+        wynik = {
+            "symbol": symbol,
+            "current_price": aktualna_cena
+        }
+        return jsonify(wynik), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=config.port, debug=True)
