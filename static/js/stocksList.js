78b0e805c733a62d8e7660ced9051a147fa76ee9
onlyJakub <jakub.wojciechowicz2@wp.pl>
2024-11-22 23:32:53 +0100

    Added buttons to the dashboard. Now u can choose what u wanna see.

---
+++ static/js/stocksList.js
@@ -0,0 +1,8 @@
+fetch('/get_list')
+.then(response => response.json())
+.then(data => {
+    const list = document.getElementById('stocks-list');
+    Object.keys(data).forEach(item => {
+        list.innerHTML += `<li><button type="button" onclick="get_data('${item}')">${item} - ${data[item].toFixed(2)}/per akcję</button></li>`;
+    });
+})
