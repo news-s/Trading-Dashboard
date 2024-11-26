e154c80973d1c09b470402ae90033a95d19bc44f
onlyJakub <jakub.wojciechowicz2@wp.pl>
2024-11-26 21:04:08 +0100

    Co-authored-by: News.exe <news-s@users.noreply.github.com>

--- static/js/bot.js
+++ static/js/bot.js
@@ -4,7 +4,24 @@ function info(){
     fetch(`/info/${window.globalVariable}`)
     .then(response => response.json())
     .then(data => {
-        contener.innerHTML = data.current_price;
-        //Dodaj tutaj jakieś ważne inforamcje o spółce
+        contener.innerHTML = "";
+        if(data == undefined){
+            contener.innerHTML = "Error fetching data";
+            return;
+        }
+        console.log(data);
+        contener.innerHTML += data.current_price;
+        //contener.innerHTML += data.longBusinessSummary;
+        contener.innerHTML += data.phone;
+        contener.innerHTML += data.uuid;
+        contener.innerHTML += `<a href="${data.website}">${data.website}</a>`;
+        contener.innerHTML += data.city;
+        contener.innerHTML += data.state;
+        contener.innerHTML += data.country;
+        contener.innerHTML += data.industry;
+        contener.innerHTML += data.sector;
+        contener.innerHTML += data.fullTimeEmployees;
+        contener.innerHTML += data.address1;
+        contener.innerHTML += data.underlyingSymbol;
     })
 }
